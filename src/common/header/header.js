87c5ff4f550c6eca88593e02402c674625b1702e
Aman <aman98072@gmail.com>
2019-07-17 07:16:34 +0530

    first commit

---
+++ src/common/header/header.js
@@ -0,0 +1,14 @@
+import React, {Component, Fragment} from 'react';
+import "./header.css";
+
+class Header extends Component{
+    render () {
+        return (
+            <Fragment>
+                <h1>Header Section</h1>
+            </Fragment>
+        );
+    }
+}
+
+export default Header;
